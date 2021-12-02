737cbf560c40599832656d407d369eee8f478dea
Eric Zhu <73148494+ericfzhu@users.noreply.github.com>
2021-12-03 10:22:08 +1100

    Added new fonts and color palette

---
+++ src/theme/palette.js
@@ -0,0 +1,67 @@
+export const light = {
+  alternate: {
+    main: '#f7faff',
+    dark: '#edf1f7',
+  },
+  cardShadow: 'rgba(23, 70, 161, .11)',
+  mode: 'light',
+  primary: {
+    main: '#A2D2FF',
+    light: '#D9F2FF',
+    dark: '#517BB7',
+    contrastText: '#fff',
+  },
+  secondary: {
+    light: '#B0E1FE',
+    main: '#3B97F9',
+    dark: '#1D57B3',
+    contrastText: 'rgba(0, 0, 0, 0.87)',
+  },
+  text: {
+    primary: '#545454',
+    secondary: '#677788',
+  },
+  divider: 'rgba(0, 0, 0, 0.12)',
+  background: {
+    paper: '#ffffff',
+    default: '#ffffff',
+    level2: '#f5f5f5',
+    level1: '#ffffff',
+  },
+};
+
+export const dark = {
+  alternate: {
+    main: '#545454',
+    dark: '#3C2A2D',
+  },
+  cardShadow: 'rgba(0, 0, 0, .11)',
+  common: {
+    black: '#000',
+    white: '#fff',
+  },
+  mode: 'dark',
+  primary: {
+    main: '#BDE0FE',
+    light: '#E4F7FE',
+    dark: '#5F85B6',
+    contrastText: '#fff',
+  },
+  secondary: {
+    light: '#FFE9EA',
+    main: '#FFC8DD',
+    dark: '#B76494',
+    contrastText: 'rgba(0, 0, 0, 0.87)',
+  },
+  text: {
+    primary: '#EEEEEF',
+    secondary: '#AEB0B4',
+  },
+  divider: 'rgba(255, 255, 255, 0.12)',
+  background: {
+    paper: '#545454',
+    default: '#545454',
+    level2: '#333',
+    level1: '#2D3748',
+  },
+};
