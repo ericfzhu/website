da831d4af5beacc947ed81f356467f3e22fe0100
Eric Zhu <73148494+ericfzhu@users.noreply.github.com>
2021-12-03 07:35:05 +1100

    Removed old content

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,50 +1,14 @@
+/* eslint-disable no-undef */
 module.exports = {
-  siteMetadata: {
-    title: `Portfolio`,
-        description: `My digital garden`,
-    author: `Eric Zhu`,
-  },
   plugins: [
-    `gatsby-plugin-react-helmet`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    `gatsby-image`,
-    `gatsby-plugin-catch-links`,
-    `gatsby-plugin-sass`,
-    `gatsby-plugin-resolve-src`,
-    `gatsby-plugin-remove-trailing-slashes`,
-    `gatsby-plugin-emotion`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `temp name`,
-        short_name: `temp`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/cat.svg`, // This path is relative to the root of the site.
-      },
-    },
-    // https://www.gatsbyjs.org/packages/gatsby-plugin-google-analytics/
-    {
-        resolve: `gatsby-plugin-google-analytics`,
-        options: {
-            trackingId: "YOUR_GOOGLE_ANALYTICS_TRACKING_ID",
-            head: true,
-        },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    'gatsby-plugin-top-layout',
+    // If you want to use styled components you should add the plugin here.
+    // 'gatsby-plugin-styled-components',
+    'gatsby-plugin-mui-emotion',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-resolve-src',
   ],
-}
+  siteMetadata: {
+    title: 'theFront - MUI Multipurpose Theme and UI Kit',
+  },
+};
