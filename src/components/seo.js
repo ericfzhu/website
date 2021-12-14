0804d99377d480da09bf1bda74b154e41d8bd609
Eric Zhu <73148494+ericfzhu@users.noreply.github.com>
2021-12-15 00:13:36 +1100

    Added twitter:card to seo

--- src/components/seo.js
+++ src/components/seo.js
@@ -33,6 +33,7 @@ const SEO = ({ title, description, image }) => {
       <meta property="og:description" content={seo.description} />
       <meta property="og:image" content={seo.image} />
 
+      <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={seo.title} />
       <meta name="twitter:description" content={seo.description} />
       <meta name="twitter:image" content={seo.image} />
