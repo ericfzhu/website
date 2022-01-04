7ae29d949d2ae3a45ee73c61e5320b46f423500a
Eric Zhu <73148494+ericfzhu@users.noreply.github.com>
2022-01-04 14:27:15 +1100

    Updated plugin manifest for new favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,7 +27,7 @@ module.exports = {
         background_color: `#545454`,
         theme_color: `#545454`,
         display: 'standalone',
-        icon: 'src/images/cat light.svg',
+        icon: 'src/images/cat light.png',
         theme_color_in_head: false,
       }
     },
