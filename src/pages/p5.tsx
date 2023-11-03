ec9a46e81aa8dc643aee2bf4aeb486db797365e7
Eric Zhu <73148494+ericfzhu@users.noreply.github.com>
2023-11-03 14:21:07 +1100

    p5

---
+++ src/pages/p5.tsx
@@ -0,0 +1,19 @@
+import P5 from "@/components/P5";
+import { useEffect, useState } from "react";
+
+export default function p5() {
+    const [canvasHeight, setCanvasHeight] = useState(500);
+    const [canvasWidth, setCanvasWidth] = useState(500);
+
+    useEffect(() => {
+        setCanvasHeight(window.innerHeight);
+        setCanvasWidth(window.innerWidth);
+    }, []);
+
+
+    return (
+        <div className="w-screen h-screen bg-black">
+            <P5 canvasHeight={canvasHeight} canvasWidth={canvasWidth}/>
+        </div>
+    );
+}
