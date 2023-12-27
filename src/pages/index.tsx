d3543d58ed3290c4c6178178d23d50f4946d2cd6
Eric Zhu <73148494+ericfzhu@users.noreply.github.com>
2023-12-27 17:41:59 +1100

    icons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -436,7 +436,7 @@ export default function HomePage() {
                         priority
                         width={1920}
                         height={1080}
-                        className={`absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 object-cover h-screen w-full ${
+                        className={`absolute top-1/2 left-1/2 transform pointer-events-none -translate-x-1/2 -translate-y-1/2 object-cover h-screen w-full ${
                             showScreensaver ? 'z-30' : '-z-20'
                         }`}
                     />
--- src/pages/library.tsx
+++ src/pages/library.tsx
@@ -5,14 +5,14 @@ export default function LibraryPage() {
     return (
         <main>
             <Head>
-                <title>图书馆</title>
-                <meta property={'og:title'} content={'图书馆'} key="title" />
+                <title>ESSENCE</title>
+                <meta property={'og:title'} content={'ESSENCE'} key="title" />
                 <meta
                     name="viewport"
                     content="width=device-width"
                     key="title"
                 />
-                <link rel="icon" href="/assets/icons/library.jpg" />
+                <link rel="icon" href="/assets/icons/ESSENCE3.jpg" />
             </Head>
             <Library />
         </main>
