f4650f8625731c83b638dc2e7344304282ff3501
Eric Zhu <73148494+ericfzhu@users.noreply.github.com>
2024-02-05 16:48:10 +1100

    black

--- src/scripts/data.py
+++ src/scripts/data.py
@@ -6,7 +6,9 @@ import requests
 from PIL import Image
 from dotenv import load_dotenv
 from io import BytesIO
+
 load_dotenv()
+
 
 def slugify(value: str) -> str:
     """
@@ -19,81 +21,63 @@ def slugify(value: str) -> str:
     Returns:
         str: The slugified string
     """
-    if re.search(r'[\u4e00-\u9fff]+', value) or re.search(r'[\u3040-\u30ff]+', value):
-        value = re.sub(r'[^\w\s-]', '', value)
-        return re.sub(r'[-\s]+', '-', value).strip('-_')
-    value = unicodedata.normalize('NFKD', str(value)).encode('ascii', 'ignore').decode('ascii')
-    value = re.sub(r'[^\w\s-]', '', value.lower())
-    return re.sub(r'[-\s]+', '-', value).strip('-_')
+    if re.search(r"[\u4e00-\u9fff]+", value) or re.search(r"[\u3040-\u30ff]+", value):
+        value = re.sub(r"[^\w\s-]", "", value)
+        return re.sub(r"[-\s]+", "-", value).strip("-_")
+    value = (
+        unicodedata.normalize("NFKD", str(value))
+        .encode("ascii", "ignore")
+        .decode("ascii")
+    )
+    value = re.sub(r"[^\w\s-]", "", value.lower())
+    return re.sub(r"[-\s]+", "-", value).strip("-_")
 
 
 def fetch_covers_data():
-    NOTION_DATABASE_ID = os.environ['NOTION_DATABASE_ID']
-    NOTION_API_TOKEN = os.environ['NOTION_API_TOKEN']
-    NOTION_VERSION = os.environ['NOTION_VERSION']
+    NOTION_DATABASE_ID = os.environ["NOTION_DATABASE_ID"]
+    NOTION_API_TOKEN = os.environ["NOTION_API_TOKEN"]
+    NOTION_VERSION = os.environ["NOTION_VERSION"]
     url = f"https://api.notion.com/v1/databases/{NOTION_DATABASE_ID}/query"
-    headers = {'Authorization': f'Bearer {NOTION_API_TOKEN}', 'Notion-Version': NOTION_VERSION, 'Content-Type': 'application/json'}
-    payload = json.dumps({
-        "filter": {
-            "or": [
-            {
-                "property": "Status",
-                "select": {
-                    "equals": "Reading"
-                }
-            },
-            {
-                "property": "Status",
-                "select": {
-                    "equals": "Finished"
-                }
-            },
-            {
-                "property": "Status",
-                "select": {
-                    "equals": "To Read"
-                }
-            }
-            ]
-        }
-    })
-    response = requests.request("POST", url, headers=headers, data=payload).json()
-    results = response.get('results', [])
-    
-    # Check if the response contains 'has_more' and 'next_cursor'
-    while response.get('has_more'):
-        # Update the payload with 'start_cursor' set to 'next_cursor'
-        payload = json.dumps({
+    headers = {
+        "Authorization": f"Bearer {NOTION_API_TOKEN}",
+        "Notion-Version": NOTION_VERSION,
+        "Content-Type": "application/json",
+    }
+    payload = json.dumps(
+        {
             "filter": {
                 "or": [
-                {
-                    "property": "Status",
-                    "select": {
-                        "equals": "Reading"
-                    }
-                },
-                {
-                    "property": "Status",
-                    "select": {
-                        "equals": "Finished"
-                    }
-                },
-                {
-                    "property": "Status",
-                "select": {
-                        "equals": "To Read"
-                    }
-                }
+                    {"property": "Status", "select": {"equals": "Reading"}},
+                    {"property": "Status", "select": {"equals": "Finished"}},
+                    {"property": "Status", "select": {"equals": "To Read"}},
                 ]
-            },
-            "start_cursor": response.get('next_cursor')
-        })
+            }
+        }
+    )
+    response = requests.request("POST", url, headers=headers, data=payload).json()
+    results = response.get("results", [])
+
+    # Check if the response contains 'has_more' and 'next_cursor'
+    while response.get("has_more"):
+        # Update the payload with 'start_cursor' set to 'next_cursor'
+        payload = json.dumps(
+            {
+                "filter": {
+                    "or": [
+                        {"property": "Status", "select": {"equals": "Reading"}},
+                        {"property": "Status", "select": {"equals": "Finished"}},
+                        {"property": "Status", "select": {"equals": "To Read"}},
+                    ]
+                },
+                "start_cursor": response.get("next_cursor"),
+            }
+        )
         # Make the request again with the updated payload
         response = requests.request("POST", url, headers=headers, data=payload).json()
         # Combine the responses
-        results.extend(response.get('results', []))
-    
-    response['results'] = results
+        results.extend(response.get("results", []))
+
+    response["results"] = results
 
     # save each title and slugified title into a json file
     library = {}
@@ -102,9 +86,18 @@ def fetch_covers_data():
         cover_url = result["properties"]["Cover"]["files"][0]["external"]["url"]
         title = result["properties"]["Name"]["title"][0]["text"]["content"]
         status = result["properties"]["Status"]["select"]["name"]
-        date_finished = None if status == "Reading" or status == "To Read" else result["properties"]["End"]["date"]["start"]
+        date_finished = (
+            None
+            if status == "Reading" or status == "To Read"
+            else result["properties"]["End"]["date"]["start"]
+        )
         author = result["properties"]["Author"]["rich_text"][0]["text"]["content"]
-        library[slugify(title)] = {"title": title, "status": status, "date_finished": date_finished, "author": author}
+        library[slugify(title)] = {
+            "title": title,
+            "status": status,
+            "date_finished": date_finished,
+            "author": author,
+        }
 
         if not os.path.exists(f"public/assets/covers/{slugify(title)}_300px.jpg"):
             image = requests.get(cover_url)
@@ -119,14 +112,21 @@ def fetch_covers_data():
                 new_height = int(new_width * height / width)
                 img = img.resize((new_width, new_height), Image.LANCZOS)
                 img = img.convert("RGB")
-                img.save(f"public/assets/covers/{slugify(title)}_300px.jpg", optimize=True, quality=85)
+                img.save(
+                    f"public/assets/covers/{slugify(title)}_300px.jpg",
+                    optimize=True,
+                    quality=85,
+                )
                 new_width = 500
                 new_height = int(new_width * height / width)
                 img = img.resize((new_width, new_height), Image.LANCZOS)
                 img = img.convert("RGB")
-                img.save(f"public/assets/covers/{slugify(title)}_500px.jpg", optimize=True, quality=85)
+                img.save(
+                    f"public/assets/covers/{slugify(title)}_500px.jpg",
+                    optimize=True,
+                    quality=85,
+                )
 
-        
         # image = requests.get(cover_url)
         # img = Image.open(f"public/assets/covers/{slugify(title)}.jpg")
         # width, height = img.size
@@ -135,21 +135,23 @@ def fetch_covers_data():
         # img = img.resize((new_width, new_height), Image.LANCZOS)
         # img.save(f"public/assets/covers/{slugify(title)}_300px.jpg", optimize=True, quality=85)
 
-    with open("src/components/data/library.json", "w", encoding='utf-8') as file:
+    with open("src/components/data/library.json", "w", encoding="utf-8") as file:
         json.dump(library, file, indent=4, ensure_ascii=False)
 
 
 def fetch_movie_data():
-    NOTION_MOVIEDB_ID = os.environ['NOTION_MOVIEDB_ID']
-    NOTION_API_TOKEN = os.environ['NOTION_API_TOKEN']
-    NOTION_VERSION = os.environ['NOTION_VERSION']
+    NOTION_MOVIEDB_ID = os.environ["NOTION_MOVIEDB_ID"]
+    NOTION_API_TOKEN = os.environ["NOTION_API_TOKEN"]
+    NOTION_VERSION = os.environ["NOTION_VERSION"]
     url = f"https://api.notion.com/v1/databases/{NOTION_MOVIEDB_ID}/query"
-    headers = {'Authorization': f'Bearer {NOTION_API_TOKEN}', 'Notion-Version': NOTION_VERSION, 'Content-Type': 'application/json'}
-    payload = json.dumps({"filter": {
-            "property": "Watched", 
-            "date": {
-                "is_not_empty": True
-            }}})
+    headers = {
+        "Authorization": f"Bearer {NOTION_API_TOKEN}",
+        "Notion-Version": NOTION_VERSION,
+        "Content-Type": "application/json",
+    }
+    payload = json.dumps(
+        {"filter": {"property": "Watched", "date": {"is_not_empty": True}}}
+    )
     response = requests.request("POST", url, headers=headers, data=payload).json()
 
     movies = {}
@@ -158,21 +160,27 @@ def fetch_movie_data():
         title = result["properties"]["Name"]["title"][0]["text"]["content"]
         cover_url = result["properties"]["Cover"]["files"][0]["external"]["url"]
         date_finished = result["properties"]["Watched"]["date"]["start"]
-        movies[slugify(title)] = {"title": slugify(title), "date_finished": date_finished}
+        movies[slugify(title)] = {
+            "title": slugify(title),
+            "date_finished": date_finished,
+        }
         if not os.path.exists(f"public/assets/movies/{slugify(title)}_300px.jpg"):
             image = requests.get(cover_url)
             if image.status_code == 200:
                 print(f"Downloading {title} -> {slugify(title)}...")
                 # with open(f"public/assets/movies/{slugify(title)}.jpg", "wb") as file:
                 #     file.write(image.content)
-                
+
                 img = Image.open(BytesIO(image.content))
                 width, height = img.size
                 new_width = 300
                 new_height = int(new_width * height / width)
                 img = img.resize((new_width, new_height), Image.LANCZOS)
-                img.save(f"public/assets/movies/{slugify(title)}_300px.jpg", optimize=True, quality=85)
-
+                img.save(
+                    f"public/assets/movies/{slugify(title)}_300px.jpg",
+                    optimize=True,
+                    quality=85,
+                )
 
         # image = requests.get(cover_url)
         # img = Image.open(BytesIO(image.content))
@@ -182,13 +190,14 @@ def fetch_movie_data():
         # img = img.resize((new_width, new_height), Image.LANCZOS)
         # img.save(f"public/assets/movies/{slugify(title)}_300px.jpg", optimize=True, quality=85)
 
-    with open("src/components/data/movies.json", "w", encoding='utf-8') as file:
+    with open("src/components/data/movies.json", "w", encoding="utf-8") as file:
         json.dump(movies, file, indent=4, ensure_ascii=False)
 
 
 def main():
     fetch_covers_data()
     fetch_movie_data()
+
 
 if __name__ == "__main__":
     main()
